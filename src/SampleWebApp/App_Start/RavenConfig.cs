a7cc6065eed92cda3b34160f9821f93dd77d0c6f
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-10-18 20:09:16 +0200

    added missing file

---
+++ src/SampleWebApp/App_Start/RavenConfig.cs
@@ -0,0 +1,26 @@
+﻿using Raven.Client;
+using Raven.Client.Embedded;
+using Raven.Database.Server;
+
+namespace SampleWebApp {
+    public static class RavenConfig {
+        public static IDocumentStore DocumentStore { get; private set; }
+
+        public static void Init() {
+            InitializeDocumentStore();
+        }
+
+        private static void InitializeDocumentStore() {
+            if (DocumentStore != null) {
+                return; // prevent misuse
+            }
+
+            NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8080);
+
+            DocumentStore = new EmbeddableDocumentStore {
+                ConnectionStringName = "RavenDB",
+                UseEmbeddedHttpServer = true
+            }.Initialize();
+        }
+    }
+}
